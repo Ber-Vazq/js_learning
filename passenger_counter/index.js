37381fb94210005559848ccdcfc3d0eabf01a0f0
Ber-Vazq <bvaq20@hotmail.com>
2025-05-05 16:03:51 -0500

    first couple challenges on scrimba

---
+++ passenger_counter/index.js
@@ -0,0 +1,12 @@
+//document.getElementById("count-el").innerText = 5
+//let firstBatch = 5
+//let secondBatch = 7
+//let count = firstBatch + secondBatch //let count be 0
+//console.log(count)
+
+let humanAge = 23
+let humanDogRatio = 7
+
+let myDogAge = humanAge * humanDogRatio
+
+console.log(myDogAge)
